102f49df37c7fface27efbdcbb7cfd801724bd66
Onur Temizkan <onur@narval.co.uk>
2021-09-02 15:35:57 +0000

    [WIP] Wrap apollo resolvers.

--- packages/tracing/src/integrations/index.ts
+++ packages/tracing/src/integrations/index.ts
@@ -3,6 +3,7 @@ export { Postgres } from './node/postgres';
 export { Mysql } from './node/mysql';
 export { Mongo } from './node/mongo';
 export { GraphQL } from './graphql';
+export { Apollo } from './apollo';
 
 // TODO(v7): Remove this export
 // Please see `src/index.ts` for more details.
