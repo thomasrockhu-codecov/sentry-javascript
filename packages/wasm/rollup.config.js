36705d9c81b1fbecb55ed2b450ce0273903bd2c5
Katie Byers <lobsterkatie@gmail.com>
2022-03-30 22:35:22 -0700

    s/makeConfigVariants/makeBundleConfigVariants

--- packages/wasm/rollup.config.js
+++ packages/wasm/rollup.config.js
@@ -1,4 +1,4 @@
-import { makeBaseBundleConfig, makeConfigVariants } from '../../rollup.config';
+import { makeBaseBundleConfig, makeBundleConfigVariants } from '../../rollup.config';
 
 const baseBundleConfig = makeBaseBundleConfig({
   input: 'src/index.ts',
@@ -8,4 +8,4 @@ const baseBundleConfig = makeBaseBundleConfig({
   outputFileBase: 'wasm',
 });
 
-export default makeConfigVariants(baseBundleConfig);
+export default makeBundleConfigVariants(baseBundleConfig);
