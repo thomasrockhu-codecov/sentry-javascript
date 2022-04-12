3a82f6bd02a816dcdb44b202260bbf35fd67ba6f
Katie Byers <lobsterkatie@gmail.com>
2022-04-12 11:11:32 -0700

    stop using `Severity` enum

--- packages/utils/test/severity.test.ts
+++ packages/utils/test/severity.test.ts
@@ -1,22 +1,16 @@
-import { SeverityLevels } from '../src/enums';
-import { severityFromString } from '../src/severity';
+import { severityFromString, validSeverityLevels } from '../src/severity';
 
 describe('severityFromString()', () => {
-  describe('normalize warn and warning', () => {
-    test('handles warn and warning', () => {
-      expect(severityFromString('warn')).toBe('warning');
-      expect(severityFromString('warning')).toBe('warning');
-    });
-    test('handles warn and warning', () => {
-      expect(severityFromString('warn')).toBe('warning');
-      expect(severityFromString('warning')).toBe('warning');
-    });
+  test("converts 'warn' to 'warning'", () => {
+    expect(severityFromString('warn')).toBe('warning');
   });
-  describe('default to log', () => {
+
+  test('defaults to log', () => {
     expect(severityFromString('foo')).toBe('log');
   });
-  describe('allows ', () => {
-    for (const level of SeverityLevels) {
+
+  test('acts as a pass-through for valid level strings', () => {
+    for (const level of validSeverityLevels) {
       expect(severityFromString(level)).toBe(level);
     }
   });
