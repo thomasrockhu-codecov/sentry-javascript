96637e2a1c706bef15fd9a395ca2780ddde8271c
Katie Byers <lobsterkatie@gmail.com>
2022-04-13 01:26:19 -0700

    configure nextjs build

--- packages/nextjs/rollup.npm.config.js
+++ packages/nextjs/rollup.npm.config.js
@@ -1,5 +1,17 @@
+import deepMerge from 'deepmerge';
+
 import { makeBaseNPMConfig, makeNPMConfigVariants } from '../../rollup.config.js';
 
+const baseConfig = makeBaseNPMConfig({
+  entrypoint: ['src/index.server.ts', 'src/index.client.ts'],
+  esModuleInterop: true,
+  watchPackages: ['integrations', 'node', 'react', 'tracing'],
+});
+
 export default makeNPMConfigVariants(
-  makeBaseNPMConfig({ watchPackages: ['integrations', 'node', 'react', 'tracing'] }),
+  deepMerge(baseConfig, {
+    // we already exclude anything listed as a dependency in `package.json`, but somewhere we import from a
+    // subpackage of nextjs and rollup doesn't automatically make the connection, so we have to exclude it manually
+    external: ['next/router'],
+  }),
 );
